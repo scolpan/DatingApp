6fad047a98ab5a7bfba5054b88d04801c2fb5e2a
Sinan Colpan <scolpan@gmail.com>
2024-04-03 21:52:18 -0400

    Section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  //@Input() usersFromHomeComponent: any; //Parent to child component.
+  @Output() cancelRegister = new EventEmitter(); //Child to parent component.
+
+  model: any = {}
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        //console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
+  } 
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
